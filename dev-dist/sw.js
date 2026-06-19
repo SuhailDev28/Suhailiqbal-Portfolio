347ba510b89eea40c7a01e4e2b60c74b03b81301
SuhailDev28 <suhailiqbal28@gmail.com>
2026-06-19 16:17:08 +0300

    Splash Screen Added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rc27jhtiu9o"
+    "revision": "0.o2uisj1tnag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
